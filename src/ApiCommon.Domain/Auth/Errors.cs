3d28721accbb57b9c265aebb9d14797be6d19b19
Petr Jilek <petrjilek@centrum.cz>
2022-05-10 19:38:16 +0200

    Add UnknownError

--- src/ApiCommon.Domain/Auth/Errors.cs
+++ src/ApiCommon.Domain/Auth/Errors.cs
@@ -8,5 +8,8 @@
         public static readonly ErrorModel BadDisplayName = new("bad_display_name");
         public static readonly ErrorModel BadPassword = new("bad_password");
         public static readonly ErrorModel PasswordsDontMatch = new("passwords_dont_match");
+
+        public static readonly ErrorModel UnkonwnError = new("unknown_error");
+
     }
 }
