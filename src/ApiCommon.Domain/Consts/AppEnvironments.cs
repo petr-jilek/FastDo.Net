4d521d36c370b0436453606d55cd4c4739fb285a
Petr Jílek <petrjilek@centrum.cz>
2022-08-02 19:31:00 +0200

    refactor

--- src/ApiCommon.Domain/Consts/AppEnvironments.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiCommon.Domain.Consts
-{
-    public static class AppEnvironments
-    {
-        public const string Development = "Development";
-        public const string Testing = "Testing";
-        public const string Production = "Production";
-    }
-}
--- src/src/ApiCommon.Domain/Consts/CorsPolicies.cs
+++ src/src/ApiCommon.Domain/Consts/CorsPolicies.cs
@@ -3,7 +3,7 @@
     public static class CorsPolicies
     {
         public const string Development = "Development_CorsPolicies";
-        public const string Testing = "Testing_CorsPolicies";
+        public const string Staging = "Staging_CorsPolicies";
         public const string Production = "Production_CorsPolicies";
     }
 }
