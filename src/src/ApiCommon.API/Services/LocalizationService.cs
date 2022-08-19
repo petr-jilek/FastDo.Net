b1dda36985ba445025dbfb5f6969b57e92445682
Petr Jílek <petrjilek@centrum.cz>
2022-08-19 14:42:44 +0200

    Add LocalizationService

---
+++ src/src/ApiCommon.API/Services/LocalizationService.cs
@@ -0,0 +1,24 @@
+﻿using ApiCommon.Application.Interfaces;
+
+namespace ApiCommon.API.Services
+{
+    public class LocalizationService : ILocalizationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LocalizationService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetLanguageCode()
+        {
+            var lang = _httpContextAccessor.HttpContext.Request.Headers["Accept-Language"];
+
+            if (string.IsNullOrEmpty(lang))
+                return "cz";
+
+            return lang;
+        }
+    }
+}
---
+++ src/src/ApiCommon.Application/Interfaces/ILocalizationService.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiCommon.Application.Interfaces
+{
+    public interface ILocalizationService
+    {
+        public string GetLanguageCode();
+    }
+}
