f3b6418ba46aca1e0c4e1ef461fbfa539eed951a
Petr Jilek <petrjilek@centrum.cz>
2022-09-22 21:53:14 +0200

    Add PasswordDbModel

---
+++ src/ApiCommon.MongoDatabase/Abstractions/PasswordDbModel.cs
@@ -0,0 +1,11 @@
+﻿using ApiCommon.Domain.Enums;
+
+namespace ApiCommon.MongoDatabase.Abstractions
+{
+    public abstract class PasswordDbModel : BaseDbModelStringId
+    {
+        public string? PasswordSalt { get; set; }
+        public string? PasswordHash { get; set; }
+        public HashMethod PasswordHashMethod { get; set; }
+    }
+}
