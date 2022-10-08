ea78f359a66680ddb8c6e78f9e8b47481ddddca1
Petr Jilek <petrjilek@centrum.cz>
2022-10-08 10:23:45 +0200

    Edit code

---
+++ src/ApiCommon.API/Application/Requests/PaginationRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCommon.API.Application.Requests
+{
+    public class PaginationRequest
+    {
+        [Required]
+        public int PageNumber { get; set; }
+
+        [Required]
+        public int PageSize { get; set; }
+    }
+}
---
+++ src/ApiCommon.API/Application/Responses/PaginationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiCommon.API.Application.Responses
+{
+    public class PaginationResponse<T>
+    {
+        public List<T>? Items { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
