134eba53fadef4c6fb47a855d1f46f0c34c6cc86
Petr Jilek <petrjilek@centrum.cz>
2022-10-26 14:41:07 +0200

    Add tests

---
+++ tests/FastDo.Net.Tests/Tests/Api/Extensions/CommonExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using FastDo.Net.Api.Extensions;
+using FastDo.Net.Tests.Models;
+
+namespace FastDo.Net.Tests.Tests.Api.Extensions
+{
+    public class CommonExtensionsTests
+    {
+        [Theory]
+        [InlineData("Test", "Test", false)]
+        [InlineData("Test", "      ", false)]
+        [InlineData("     ", "Test", false)]
+        [InlineData("Test", "", true)]
+        [InlineData("", "Test", true)]
+        [InlineData(null, "Test", true)]
+        [InlineData("Test", null, true)]
+        public void IsAnyStringNullOrEmpty_Test(string name, string description, bool expectedResult)
+        {
+            var testClass = new TestClass() { Name = name, Description = description };
+
+            var result = testClass.IsAnyStringNullOrEmpty();
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ tests/FastDo.Net.Tests/Tests/Api/Extensions/ServiceInjectionExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using FastDo.Net.Api.Extensions;
+using FastDo.Net.Tests.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FastDo.Net.Tests.Tests.Api.Extensions
+{
+    public class ServiceInjectionExtensionsTests
+    {
+        [Fact]
+        public void Ok()
+        {
+            var name = "Test";
+            var description = "Test";
+
+            var inMemorySettings = new Dictionary<string, string> {
+                {"TestClass:Name", name},
+                {"TestClass:Description", description},
+            };
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSettings<TestClass>(configuration);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            using var scope = serviceProvider.CreateScope();
+            var testClass = scope.ServiceProvider.GetService<TestClass>();
+
+            Assert.Equal(name, testClass.Name);
+            Assert.Equal(description, testClass.Description);
+        }
+    }
+}
