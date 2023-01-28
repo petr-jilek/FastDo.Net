d6994a13c7d2dbd2f6d69f1b734f70ed8e6a5aec
Petr Jilek <petr.jilek156@gmail.com>
2023-01-28 13:38:52 +0100

    Add validation attribute

---
+++ src/FastDo.Net/Api/Validators/MustHaveOneElementAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace FastDo.Net.Api.Validators
+{
+    public class MustHaveOneElementAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            return value is IList { Count: > 0 };
+        }
+    }
+}
